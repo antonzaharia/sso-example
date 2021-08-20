32620cd5417c0232a81a80d1853415091b50013c
Anton Zaharia <zaharia_anton@yahoo.com>
2021-08-20 11:56:59 +0100

    store test 1 - refactor & load user

--- store-test1/app/controllers/homes_controller.rb
+++ store-test1/app/controllers/homes_controller.rb
@@ -1,21 +1,37 @@
 class HomesController < ApplicationController
+  AUTH_SERVER = 'http://localhost:3000'.freeze
+  HEADERS = { content_type: :json, accept: :json }.freeze
+
   def index
-    if params[:token] == 'no-user'
-      session[:user_id] = nil
-      @print = params[:token]
-    elsif params[:token].present?
-      url = "http://localhost:3000/verify-token?redirect_url=#{request.original_url}"
-      payload = { token: params[:token] }.to_json
-      result = RestClient.post(url, payload, { content_type: :json, accept: :json })
-      @user = JSON.parse(result)['user']
-      session[:user_id] = @user['id']
-    else
-      redirect_to "http://localhost:3000?redirect_url=#{request.original_url}"
-    end
+    session[:user_id].present? ? load_user : handle_token
   end
 
   def logout
     session[:user_id] = nil
-    redirect_to "http://localhost:3000/logout?redirect_url=#{request.original_url}"
+    redirect_to "#{AUTH_SERVER}/logout?redirect_url=#{request.base_url}"
+  end
+
+  private
+
+  def handle_token
+    if params[:token] == 'no-user'
+      session[:user_id] = nil
+      @print = params[:token]
+    elsif params[:token].present?
+      url = "#{AUTH_SERVER}/verify-token"
+      payload = { token: params[:token] }.to_json
+      result = RestClient.post(url, payload, HEADERS)
+      @user = JSON.parse(result)['user']
+      session[:user_id] = @user['id']
+    else
+      redirect_to "#{AUTH_SERVER}?redirect_url=#{request.original_url}"
+    end
+  end
+
+  def load_user
+    url = "#{AUTH_SERVER}/load-user"
+    payload = { id: session[:user_id] }.to_json
+    result = RestClient.post(url, payload, HEADERS)
+    @user = JSON.parse(result)['user']
   end
 end
